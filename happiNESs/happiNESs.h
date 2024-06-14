71c92091ac41edc7ed838f4ffdbb01471e69fac2
quephird <danielle_kefford@hotmail.com>
2024-06-14 10:47:34 -0700

    Initial Commit

---
+++ happiNESs/happiNESs.h
@@ -0,0 +1,18 @@
+//
+//  happiNESs.h
+//  happiNESs
+//
+//  Created by Danielle Kefford on 6/14/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for happiNESs.
+FOUNDATION_EXPORT double happiNESsVersionNumber;
+
+//! Project version string for happiNESs.
+FOUNDATION_EXPORT const unsigned char happiNESsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <happiNESs/PublicHeader.h>
+
+
