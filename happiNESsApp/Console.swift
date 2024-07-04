42d5cf82998593d28127a744370dbaaa47682dc9
quephird <danielle_kefford@hotmail.com>
2024-07-03 20:37:25 -0700

    Added some more comments to `Console`.

--- happiNESsApp/Console.swift
+++ happiNESsApp/Console.swift
@@ -13,16 +13,26 @@ import SwiftUI
     static let frameRate = 60
     static let clockRate = 14_000
 
-    var displayLink: Timer!
+    var displayTimer: Timer!
 
-    // NOTA BENE: Only the screenBuffer is actually ever changing (for now)
+    // NOTA BENE: We don't want the screen updated every single time something inside
+    // this class, which is the one being observed by `ContentView`, changes. We're only
+    // really interested in changes to `Console`'s state such that at least of the underlying
+    // pixels has changed, namely any of the elements of  `screenBuffer`.
     @ObservationIgnored var cpu: CPU = CPU()
     var screenBuffer: [NESColor] = [NESColor](repeating: .black, count: 32*32)
 
     internal init() {
         cpu.load(program: CPU.gameCode)
         cpu.reset()
-        displayLink = Timer.scheduledTimer(timeInterval: 1.0/TimeInterval(Self.frameRate), target: self, selector: #selector(runForOneFrame), userInfo: nil, repeats: true)
+
+        // This sets up a timer which will call `runForOneFrame()` every time it fires.
+        self.displayTimer = Timer.scheduledTimer(
+            timeInterval: 1.0/TimeInterval(Self.frameRate),
+            target: self,
+            selector: #selector(runForOneFrame),
+            userInfo: nil,
+            repeats: true)
     }
 
     @objc func runForOneFrame() {
