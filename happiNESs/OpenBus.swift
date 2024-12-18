6ca60e3741702d1006c17ad607775a7f68bc8068
quephird <danielle_kefford@hotmail.com>
2024-12-18 14:42:38 -0800

    Added a comment and adjusted the figure for reset decay cycles.

--- happiNESs/OpenBus.swift
+++ happiNESs/OpenBus.swift
@@ -6,7 +6,19 @@
 //
 
 public struct OpenBus {
-    static let resetDecayCycles: Int = 4_288_392
+    // Technically, this isn't _quite_ right as each bit supposedly
+    // independently decays, but for the time being we're modeling
+    // the open bus as byte that decays all at once. The number of
+    // cycles that the byte "lasts" without refreshing is computed
+    // from the results of experiments done by enthusiasts, who have
+    // measure the amount of time to be about 600 milliseconds.
+    // This amount of time translates to the number of PPU cycles in
+    // in the following manner:
+    //
+    // .6 sec / (1000/60 frames/sec) = 36 frames
+    // 1 frame = 341*262 = 89342 PPU cycles
+    // 36 frames * 89342 cycles/frame = 3216312 cycles
+    static let resetDecayCycles: Int = 3_216_312
 
     public var value: Register = 0x00
     private var decayCycles: Int = Self.resetDecayCycles
