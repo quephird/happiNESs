07b26a2d4d3899303cadeccecefa491cd8be63a9
quephird <danielle_kefford@hotmail.com>
2024-12-19 14:03:18 -0800

    Added nestest.nes test ROM to suite.

--- happiNESsTests/ROMTests.swift
+++ happiNESsTests/ROMTests.swift
@@ -6,10 +6,33 @@
 //
 
 import XCTest
-@testable import happiNESs
+import happiNESs
 
 final class ROMTests: XCTestCase {
     let cpu = CPU(bus: Bus())
+
+    func testNestest() throws {
+        guard let romUrl = Bundle(for: ROMTests.self).url(forResource: "nestest", withExtension: "nes") else {
+            XCTFail("ROM file could not be found")
+            return
+        }
+        let romData: Data = try Data(contentsOf: romUrl)
+        let cartridge = try Cartridge(romData: romData,
+                                      interruptible: cpu.bus)
+
+        cpu.loadCartridge(cartridge: cartridge)
+        cpu.reset()
+        cpu.programCounter = 0xC000
+
+        while cpu.programCounter != 0xC66E {
+            let _ = cpu.executeInstruction()
+        }
+
+        let officialOpcodeStatus = self.cpu.readByte(address: 0x0002)
+        let unofficialOpcodeStatus = self.cpu.readByte(address: 0x0003)
+        XCTAssertEqual(officialOpcodeStatus, 0x00, "Official opcodes failed")
+        XCTAssertEqual(unofficialOpcodeStatus, 0x00, "Official opcodes failed")
+    }
 
     func testOfficialOnly() throws {
         try testBlarggRom(romName: "official_only", cpu: self.cpu)
