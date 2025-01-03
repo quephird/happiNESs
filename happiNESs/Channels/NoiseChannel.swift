b61b1d971d91e78353763e0a800b59558669546b
quephird <danielle_kefford@hotmail.com>
2025-01-02 22:22:43 -0800

    Small refactoring in noise channel.

--- happiNESs/Channels/NoiseChannel.swift
+++ happiNESs/Channels/NoiseChannel.swift
@@ -5,38 +5,29 @@
 //  Created by Danielle Kefford on 10/24/24.
 //
 
-enum NoiseControlFlag {
-    case lengthCounterEnabled
-    case envelopeLoop
-}
-
 public struct NoiseChannel {
     static let timerPeriods: [UInt16] = [
         4, 8, 16, 32, 64, 96, 128, 160, 202, 254, 380, 508, 762, 1016, 2034, 4068,
     ]
 
     // TODO: Need comments explaining all these fields!!!
-    private var controlFlag: NoiseControlFlag = .lengthCounterEnabled
     private var constantVolumeFlag: Bool = false
     private var constantVolume: UInt8 = 0x00
     private var mode: Int = 1
     private var shiftRegister: UInt16 = 0x0001
-    private var timerPeriod: UInt16 = 0x0000
-    private var timerValue: UInt16 = 0x0000
 
     private var dutyIndex: Int = 0
     public var lengthCounter: LengthCounter = LengthCounter()
+    private var timer: Timer = Timer()
     private var envelope: Envelope = Envelope()
 
     mutating public func reset() {
-        self.controlFlag = .lengthCounterEnabled
         self.constantVolumeFlag = false
         self.constantVolume = 0x00
         self.mode = 1
         self.shiftRegister = 0x0001
-        self.timerPeriod = 0x0000
-        self.timerValue = 0x0000
         self.lengthCounter.reset()
+        self.timer.reset()
         self.envelope.reset()
         self.dutyIndex = 0
     }
@@ -56,7 +47,7 @@ extension NoiseChannel {
 
     mutating public func writeLoopAndPeriod(byte: UInt8) {
         self.mode = byte[.noiseMode] == 1 ? 6 : 1
-        self.timerPeriod = Self.timerPeriods[Int(byte[.noisePeriod])]
+        self.timer.period = Self.timerPeriods[Int(byte[.noisePeriod])] - 1
     }
 
     mutating public func writeLength(byte: UInt8) {
@@ -65,15 +56,11 @@ extension NoiseChannel {
     }
 
     mutating public func stepTimer() {
-        if self.timerValue == 0 {
-            self.timerValue = self.timerPeriod
-
+        if self.timer.step() {
             let bit1 = self.shiftRegister & 1
             let bit2 = (self.shiftRegister >> self.mode) & 1
             self.shiftRegister >>= 1
             self.shiftRegister |= (bit1 ^ bit2) << 14
-        } else {
-            self.timerValue -= 1
         }
     }
 
